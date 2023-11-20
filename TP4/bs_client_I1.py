63466d1f72841d5456844b56343af60fbf329d42
matastrals <matastral@gmail.com>
2023-11-20 11:11:59 +0100

    continue on vscode

--- TP4/bs_client_I1.py
+++ TP4/bs_client_I1.py
@@ -1,16 +1,22 @@
 import socket
 import sys
 
-host = "10.33.79.254" 
-port = 13337              
+host = "10.33.76.234"
+port = 13337
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.connect((host, port))
-
 try:
-    s.sendall(b"Meooooo !")
+    s.connect((host, port))
+except:
+    print(f"Error when trying to connect at {host} sur le port {port}")
+    exit(1)
+else:
+    print(f"Connecté avec succès au serveur {host} sur le port {port}" )
+
+message = input("Que veux-tu envoyer au serveur : ")
+try:
+    s.sendall(f"{message}")
     data = s.recv(1024)
 
 except Exception as e:
