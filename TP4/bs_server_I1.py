e52dbf7acb9ba93a3408a5f6ad00e3c08f7fbd73
mmattis <mattis.milochau@ynov.com>
2023-11-24 16:08:18 +0100

    try part2-3

--- TP4/bs_server_I1.py
+++ TP4/bs_server_I1.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import re
 
 host="10.33.76.234"
 port=13_337
@@ -18,18 +19,26 @@ else:
 
 while True:
     data = conn.recv(1024)
-    if data.decode().__contains__("meo"):
-        message = "Meo a toi confrere."
-        conn.sendall(message.encode())
-        break
-    elif data.decode().__contains__("waf"):
-        message = "ptdr t ki"
-        conn.sendall(message.encode())
-        break
+    if type(data.decode) == str and re.search(r'meo|waf', data.decode()):
+        if data.decode().__contains__("meo"):
+            message = "Meo a toi confrere."
+            conn.sendall(message.encode())
+            break
+        elif data.decode().__contains__("waf"):
+            message = "ptdr t ki"
+            conn.sendall(message.encode())
+            break
+        else:
+            message = "Mes respects humble humain."
+            conn.sendall(message.encode())
+            break
     else:
-        message = "Mes respects humble humain."
-        conn.sendall(message.encode())
-        break
-        
+        if type(data.decode()) != str:
+            raise TypeError("Ici on veut que des strings !")
+        elif re.search(r'meo|waf', data.decode()) == False:
+            raise TypeError("On ne veut pas d'humain !")
+        else:
+            raise TypeError("Pourquoi une erreur ????")
+            
 conn.close()
 sys.exit(0)
