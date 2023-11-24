d714397a1f8abb0e282664731f6e55bbffaf0e57
mmattis <mattis.milochau@ynov.com>
2023-11-24 16:11:02 +0100

    try part2-3

--- TP4/bs_server_I1.py
+++ TP4/bs_server_I1.py
@@ -38,7 +38,7 @@ while True:
         elif not re.search(r'meo|waf', data.decode()):
             raise TypeError("On ne veut pas d'humain !")
         else:
-            print(data.decode())
+            print(data.decode(), re.search(r'meo|waf', data.decode()))
             raise TypeError("Pourquoi une erreur ????")
             
 conn.close()
