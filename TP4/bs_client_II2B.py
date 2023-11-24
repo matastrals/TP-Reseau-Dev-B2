c453df591e335be3bc60c9b0bd8bd348f6b41f05
mmattis <mattis.milochau@ynov.com>
2023-11-24 20:03:58 +0100

    Partie 2-B

--- TP4/bs_client_II2B.py
+++ TP4/bs_client_II2B.py
@@ -2,19 +2,24 @@ import socket
 import sys
 import re
 import logging
+import os
 
 host = "192.168.1.98"
 port = 13337
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', handlers=[logging.FileHandler("/var/log/bs_server/bs_server.log"), logging.StreamHandler(sys.stdout)],)
-logging.basicConfig(level=logging.ERROR, format='\033[0m%(asctime)s - \033[91m%(levelname)s\033[0m - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', handlers=[logging.FileHandler("/var/log/bs_server/bs_server.log"), logging.StreamHandler(sys.stdout)],)
+os.makedirs("./logs", exist_ok=True)
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', handlers=[logging.FileHandler("./logs/bs_client.log"), logging.StreamHandler(sys.stdout)],)
+logging.basicConfig(level=logging.ERROR, format='\033[0m%(asctime)s - \033[91m%(levelname)s\033[0m - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', handlers=[logging.FileHandler("./logs/bs_client.log"), logging.StreamHandler(sys.stdout)],)
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     s.connect((host, port))
 except:
-    logging.info(f"Connexion réussie à {host}:{port}")
+    logging.error(f"Impossible de se connecter au serveur {host} sur le port {port}")
+    print(f"Impossible de se connecter au serveur {host} sur le port {port}")
     exit(1)
 else:
     logging.info(f"Connexion réussie à {host}:{port}")
