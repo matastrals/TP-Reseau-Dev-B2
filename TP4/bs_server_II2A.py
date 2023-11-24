049995c7cc3c459ea9eaae9fde32083a9d39a561
mmattis <mattis.milochau@ynov.com>
2023-11-24 19:39:31 +0100

    try linux

--- TP4/bs_server_II2A.py
+++ TP4/bs_server_II2A.py
@@ -9,7 +9,7 @@ import select
 os.makedirs("/var/log/bs_server", exist_ok=True)
 
 # logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', handlers=[logging.FileHandler("/var/log/bs_server/bs_server.log"), logging.StreamHandler(sys.stdout)],)
-logging.basicConfig(level=logging.WARNING, format='\033[93m %(levelname)s - \033[0m %(message)s', )
+logging.basicConfig(level=logging.WARNING, format='\033[93m %(levelname)s \033[0m - %(message)s', )
 
 host="10.33.76.234"
 
