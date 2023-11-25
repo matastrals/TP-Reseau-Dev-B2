57492be40df9d966be09bbaa4b91add9e7931644
mmattis <mattis.milochau@ynov.com>
2023-11-25 21:50:29 +0100

    try on linux

---
+++ TP4/bs_client_III.py
@@ -0,0 +1,51 @@
+import socket
+import sys
+import re
+import logging
+import os
+
+host = "10.33.76.234"
+port = 13337
+
+os.makedirs("./logs", exist_ok=True)
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', handlers=[logging.FileHandler("./logs/bs_client.log"), logging.StreamHandler(sys.stdout)],)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.connect((host, port))
+except:
+    logging.error(f"Impossible de se connecter au serveur {host} sur le port {port}")
+    print(f"Impossible de se connecter au serveur {host} sur le port {port}")
+    exit(1)
+else:
+    logging.info(f"Connexion réussie à {host}:{port}")
+print("Bienvenue dans la calculatrice en réseau !")
+print("Vous pouvez envoyer un calcul simple dans le format suivant \"1+1\".")
+print("Vous pouvez utilitsez les symbols \"+, -, *\"")
+print("Les chiffres ne peuvent excéder 10 000 (valeur négative et positive)\n")
+
+while True:
+    message = input("Que veux-tu envoyer au serveur : ")
+    if type(message) != str:
+        raise TypeError("Ici on veut que des strings !")
+    elif not bool(message.search(r'^(-?\d{1,4})\s*([-+*])\s*(-?\d{1,4})$')):
+        raise TypeError("Ton calcule n'est pas bon !")
+    else:
+        if message == "stop":
+            s.close()
+            sys.exit(0)
+    try:
+        logging.info(f"Message envoyée au serveur {host} : {message}")
+        s.sendall(message.encode())
+        data = s.recv(1024)
+        logging.info(f"Réponse reçue du serveur {host} : {data.decode()}")
+    except Exception as e:
+        print(e)
+        s.close()
+        sys.exit(1)
+
+print(data.decode())
+s.close()
+sys.exit(0)
---
+++ TP4/bs_server_III.py
@@ -0,0 +1,78 @@
+import socket
+import sys
+import argparse
+import logging
+import datetime
+import os
+import select
+
+os.makedirs("/var/log/calculator_server", exist_ok=True)
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', handlers=[logging.FileHandler("/var/log/bs_server/bs_server.log"), logging.StreamHandler(sys.stdout)],)
+
+host="10.33.76.234"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--port", action="store", help="open on a specific port")
+args = parser.parse_args()
+
+port = args.port
+
+if (port == None):
+    port = 13337
+
+if (port < 0 or port > 65535):
+    raise ValueError("ERROR Le port spécifié n'est pas un port possible (de 0 à 65535).")
+
+if (port > 0 and port < 1024):
+    raise ValueError("ERROR Le port spécifié est un port privilégié. Spécifiez un port au dessus de 1024.")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((host, port))  
+s.listen(1)
+logging.info(f'Le serveur tourne sur {host}:{port}')
+
+# Configuration du timeout
+s.settimeout(10)  # Timeout de 10 secondes
+
+s.setblocking(False)
+
+# Initialisation de lastTime
+lastTime = datetime.datetime.now()
+
+while True:
+    # Utiliser select pour attendre la connexion avec un timeout
+    readable, _, _ = select.select([s], [], [], 10)
+    if s in readable:
+        conn, addr = s.accept()
+        print(f"Connexion établie avec {addr}")
+        # Faire quelque chose avec la connexion, si nécessaire
+        break
+    period = datetime.datetime.now()
+    if (period - lastTime).total_seconds() >= 60:
+        logging.warning('Aucun client depuis plus de 60 secondes.')
+        lastTime = period
+
+logging.info(f'Un client {addr[0]} s\'est connecté.')
+lastTime = datetime.datetime.now()
+
+while True:
+    # Reçoit les données du client
+    data = conn.recv(1024)
+    if data:
+        logging.info(f'Le client {addr[0]} a envoyé {data.decode()}')
+        conn.sendall(eval(data.decode))
+        logging.info(f'Réponse envoyée au client {addr[0]} : {data.decode}')
+    else:
+        # Ferme le serveur si le client n'envoie pas de données pendant 60 seconds
+        period = datetime.datetime.now()
+        if (period - lastTime).total_seconds() >= 60:
+            logging.warning('Aucune données reçu pendant plus de 60 secondes.')
+            lastTime = period
+            break
+
+logging.info(f'Fermeture du serveur.')
+conn.close()
+sys.exit(0)
+
