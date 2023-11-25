4bfd0af27a765baf9a0f7570f62cf9a8b3d604ff
mmattis <mattis.milochau@ynov.com>
2023-11-25 22:43:09 +0100

    try on linux

--- TP4/bs_client_III.py
+++ TP4/bs_client_III.py
@@ -39,6 +39,7 @@ while True:
                 s.sendall(message.encode())
                 data = s.recv(1024)
                 logging.info(f"Réponse reçue du serveur {host} : {data.decode()}")
+                print(data.decode())
                 break
             except Exception as e:
                 print(e)
